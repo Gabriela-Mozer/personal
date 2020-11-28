ced729725bfdc1da910fc7d5695b8e0043741e12
Gabriela Mozer <gabrielamozer1966@gmail.com>
2020-11-28 18:58:50 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,10 @@
+const firstName = "Gabriela";
+const age = 22;
+console.log(firstName);
+console.log(age);
+console.log(`Heja, nazywam się ${firstName} i mam ${age} lata`);
+
+alert("Eluwina");
+
+const heading = document.querySelector('.main-heading--js');
+heading.innerHTML= `Heja, nazywam się ${firstName} i mam ${age} lata`
