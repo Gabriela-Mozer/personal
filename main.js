e8aa8a7a0c2a729998c5fbf23b3cf9de3e4eebf7
Gabriela Mozer <gabrielamozer1966@gmail.com>
2020-11-28 22:16:01 +0100

    new tekst and js

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ console.log(firstName);
 console.log(age);
 console.log(`Heja, nazywam się ${firstName} i mam ${age} lata`);
 
-alert("Eluwina");
+alert("Witam w moim pierwszym projekcie WTF");
 
 const heading = document.querySelector('.main-heading--js');
-heading.innerHTML= `Heja, nazywam się ${firstName} i mam ${age} lata`
+console.log(heading);
+
+const element = document.querySelector('.extra-element--js');
+element.innerHTML =`Inna wartość`
+// console.log(element);
